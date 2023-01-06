efafe007058c949354ea74dd3e46880e3a58fa41
Gagan Malvi <malvigagan@gmail.com>
2023-01-06 23:18:38 +0530

    vm-mgr-cli: move to libvirt api
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- virt/utils.py
+++ virt/utils.py
@@ -9,7 +9,9 @@
 import libvirt
 import subprocess
 import time
-import constants
+import virt.constants
+from xml.dom import minidom
+from xml.etree import ElementTree
 
 # Utilities to interact with libvirt to retrieve
 # information about the VM including
@@ -19,34 +21,58 @@ import constants
 # - Network usage
 
 # Utilities to retrieve information about the VM
-# using virsh.
+# using libvirt.
 
+# Get all VM names
 def get_all_vm_names():
     """Returns a list of all the VMs running on the host"""
     vm_names = []
     try:
-        vm_names = subprocess.check_output(['virsh', 'list', '--name']).decode().split()
+        vm_names = libvirt.open(virt.constants.QEMU_PATH).listAllDomains()
+        if len(vm_names) != 0: 
+            vm_names = [vm.name() for vm in vm_names]
+        print(vm_names)
     except subprocess.CalledProcessError as e:
         print(e)
     return vm_names
 
+# Get VM information
+# @param: vm_name - Name of the VM
 def get_vm_info(vm_name: str):
     """Returns a dictionary containing the data about the VM"""
     vm_data = {}
     try:
-        data = subprocess.check_output(['virsh', 'dominfo', vm_name]).decode().split()
-        vm_data['id'] = data[1]
-        vm_data['name'] = data[3]
-        vm_data['state'] = data[5]
-        vm_data['max_mem'] = data[18]
-        vm_data['cpus'] = data[12]
-        vm_data['cpu_time'] = data[15]
+        conn = libvirt.open(virt.constants.QEMU_PATH)
+        if not conn: 
+            print('Failed to open connection to qemu:///system')
+            exit(1)
+        dom = conn.lookupByName(vm_name)
+        if not dom: 
+            print('Failed to find the domain ' + vm_name)
+            exit(1)
+        raw_xml = dom.XMLDesc()
+        xml = minidom.parseString(raw_xml)
+        vm_data['name'] = dom.name()
+        vm_data['uuid'] = dom.UUIDString()
+        vm_data['state'] = dom.state()[0]
+        vm_data['max_memory'] = dom.maxMemory()
+        vm_data['memory'] = dom.memoryStats()['rss']
+        vm_data['vcpus'] = dom.maxVcpus()
+        vm_data['cpu_time'] = dom.getCPUStats(True)[0]['cpu_time']
+        vm_data['os_type'] = xml.getElementsByTagName('os')[0].getElementsByTagName('type')[0].firstChild.data
+        vm_data['os_arch'] = xml.getElementsByTagName('os')[0].getElementsByTagName('type')[0].getAttribute('arch')
+        vm_data['os_machine'] = xml.getElementsByTagName('os')[0].getElementsByTagName('type')[0].getAttribute('machine')
+        vm_data['os_boot'] = xml.getElementsByTagName('os')[0].getElementsByTagName('boot')[0].getAttribute('dev')
     except subprocess.CalledProcessError as e:
         print(e)
     return vm_data
 
+# Get VM data live
+# @param: delay - Delay between each data collection
+#         dataset - List to store the data
+#         vm_name - Name of the VM
 def get_vm_data_live(delay: int, dataset: list, vm_name: str):
-    conn = libvirt.open(constants.QEMU_PATH)
+    conn = libvirt.open(virt.constants.QEMU_PATH)
     if conn == None:
         print('Failed to open connection to qemu:///system')
         exit(1)
@@ -57,19 +83,35 @@ def get_vm_data_live(delay: int, dataset: list, vm_name: str):
         exit(1)
     
     running = True
+    tree = ElementTree.fromstring(dom.XMLDesc())
 
     while running: 
+        # Check if the VM is still running
+        if dom.state()[0] != libvirt.VIR_DOMAIN_RUNNING:
+            running = False
+            break
+
         # Get CPU and memory usage
         cpu_usage = dom.getCPUStats(True)[0]['cpu_time']
         mem_usage = dom.memoryStats()['rss']    
 
         # Get current network usage in bytes
-        interface_stats = dom.interfaceStats()
-        net_usage = interface_stats['rx_bytes'] + interface_stats['tx_bytes']
+        tree = ElementTree.fromstring(dom.XMLDesc())
+        iface = tree.find('devices/interface/target').get('dev')
+        interface_stats = dom.interfaceStats(iface)
+        net_usage = {
+            'rx_bytes': interface_stats[0],
+            'rx_packets': interface_stats[1],
+            'rx_errs': interface_stats[2],
+            'rx_drop': interface_stats[3],
+            'tx_bytes': interface_stats[4],
+            'tx_packets': interface_stats[5],
+            'tx_errs': interface_stats[6],
+            'tx_drop': interface_stats[7]
+        }
 
         # Store the data in a tuple
-        data = (cpu_usage, mem_usage, net_usage)
-        print(data)
+        data = [cpu_usage, mem_usage, net_usage]
 
         # Append the data to the dataset
         dataset.append(data)
